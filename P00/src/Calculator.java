bb7bf992d2be139d65f00449706c4159ad155262
21008188 <21008188@LAPTOP-EE16DBT7>
2022-06-01 12:19:42 +0800

    first push to P00 respository in GitHbu

---
+++ P00/src/Calculator.java
@@ -0,0 +1,27 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21008188, 31/5/22 12.42pm
+ */
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+
+}
---
+++ P00/src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21008188, 31/5/22 12.42pm
+ */
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Java Program");
+	}
+
+}
